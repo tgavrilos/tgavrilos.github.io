0152353afeb0eb58ca27d7f34fa01dd7531c5beb
Jeffrey Alan Lindsey <jeffal@yandex>
2013-09-28 11:36:05 -0500

    updates js and css, added carousel

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+function autorun()
+{
+	var today = new Date()
+		, birthday = new Date("1990/04/09")
+		, age = Math.abs(today.getFullYear() - birthday.getFullYear());
+
+	$("li")
+		.bind({
+			"reposition": function() {
+				var degrees = $(this).data("roundabout").degrees
+					, roundaboutBearing = $(this).parent().data("roundabout").bearing
+					, rotateY = Math.round(roundaboutBearing - degrees);
+				
+				$(this).css({
+					"-webkit-transform": "perspective(2000) rotateY(" + rotateY + "deg)",
+					"-moz-transform": "perspective(2000) rotateY(" + rotateY + "deg)",
+					"transform": "perspective(2000) rotateY(" + rotateY + "deg)"
+				});
+			}
+		})
+	
+	$("ul").roundabout({
+		autoplay: true,
+		minScale: 0.7,
+		easing: "easeOut",
+		duration: 1600
+	});
+
+	console.log("Thanks for visiting tomgavrilos.com - The portfolio of Tom Gavrilos. I'm a " + age + " year old product designer presently residing in Detroit, MI.");
+}
+if (window.addEventListener) window.addEventListener("load", autorun, false);
+else if (window.attachEvent) window.attachEvent("onload", autorun);
+else window.onload = autorun;
